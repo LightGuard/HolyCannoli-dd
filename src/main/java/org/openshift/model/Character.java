b359dec13cf3e98ef8332a973b07962465002b86
LightGuard <lightguard.jp@gmail.com>
2018-01-18 15:08:56 -0700

    Initial commit

---
+++ src/main/java/org/openshift/model/Character.java
@@ -0,0 +1,89 @@
+package org.openshift.model;
+
+import java.util.*;
+import static java.util.Arrays.asList;
+
+/**
+ * Created by spousty on 8/22/14.
+ */
+public class Character {
+    Random rand = new Random();
+    private int  hitpoints, goldpieces;
+    private final HashSet<String> attributeNames =  new HashSet<String>(asList("strength", "intelligence", "charisma", "wisdom", "dexterity", "constitution"));
+    private List<String> races =  asList( "Human", "Dwarf", "Halfling", "Elf", "Gnome", "Half-Elf", "Half-Orc", "Nerd");
+    private List<String> playerClass =  asList("Warrior", "Ranger", "Paladin", "Cleric", "Monk", "Wizard", "Thief", "Assassin", "Druid", "Wizard", "Coder");
+    private HashMap allAttributes = new HashMap(18);
+
+    private final int MAX_GOLD = 200;
+    private final int MAX_HIT = 9;
+
+    public Character(){
+
+        this(UUID.randomUUID().toString());
+
+    }
+
+    public Character(String name) {
+        allAttributes.put("hitpoints", rand.nextInt(MAX_HIT) + 1);
+        allAttributes.put("goldpieces", rand.nextInt(MAX_GOLD) + 5);
+        setAttributes(allAttributes);
+        addRaceOrClass("race", races, allAttributes);
+        addRaceOrClass("playerclass", playerClass, allAttributes);
+        allAttributes.put("name", name);
+
+    }
+
+    private void setAttributes(HashMap aAttributes){
+        //Roll up the normal attributes
+        for (String name: attributeNames){
+            aAttributes.put(name, rand.nextInt(17) + 1);
+        }
+
+    }
+
+    private void addRaceOrClass(String attName, List<String> raceOrClass, HashMap aAttributes){
+
+        int listSize = raceOrClass.size();
+        aAttributes.put(attName, raceOrClass.get(rand.nextInt(listSize)));
+    }
+
+    public HashMap getAllAttributes(){
+        return allAttributes;
+    }
+
+    public void setName(String name){
+        allAttributes.put("name", name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Character character = (Character) o;
+
+        if (goldpieces != character.goldpieces) return false;
+        if (hitpoints != character.hitpoints) return false;
+        if (allAttributes != null ? !allAttributes.equals(character.allAttributes) : character.allAttributes != null)
+            return false;
+        if (attributeNames != null ? !attributeNames.equals(character.attributeNames) : character.attributeNames != null)
+            return false;
+        if (playerClass != null ? !playerClass.equals(character.playerClass) : character.playerClass != null)
+            return false;
+        if (races != null ? !races.equals(character.races) : character.races != null) return false;
+        if (rand != null ? !rand.equals(character.rand) : character.rand != null) return false;
+
+        return true;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int result = rand != null ? rand.hashCode() : 0;
+        result = 31 * result + hitpoints;
+        result = 31 * result + goldpieces;
+        result = 31 * result + (allAttributes != null ? allAttributes.hashCode() : 0);
+        result = 31 * rand.nextInt();
+        return result;
+    }
+}
---
+++ src/main/java/org/openshift/webservice/Generator.java
@@ -0,0 +1,113 @@
+package org.openshift.webservice;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.util.JsonSerialization;
+import org.openshift.model.*;
+import org.openshift.model.Character;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+
+/**
+ * Created by spousty on 8/22/14.
+ */
+
+@Path("/char")
+public class Generator {
+
+    private org.openshift.model.Character character = new Character();
+
+    @Context
+    private SecurityContext securityContext;
+
+    @GET()
+    @Produces("application/json")
+    public HashMap MakeACharacter(){
+        return character.getAllAttributes();
+    }
+
+    @GET()
+    @Produces("application/json")
+    @Path("{name}")
+    public HashMap MakeACharacterWithAName(@PathParam("name") String name){
+
+        character.setName(name);
+        return character.getAllAttributes();
+
+    }
+
+    @GET()
+    @Produces("application/json")
+    @Path("dd")
+    public String MakeACharacterForMongo(@Context HttpServletRequest request) throws Exception{
+
+        ///get the userid from keycloak here and use it for the name
+        String name = securityContext.getUserPrincipal().getName();
+        character.setName(name);
+
+        // turn the character to JSON
+
+
+        //now send the character on to the Mongo Service
+        //Get the token
+        KeycloakSecurityContext mySecurityContext = (KeycloakSecurityContext)request.getAttribute(KeycloakSecurityContext.class.getName());
+        String token = mySecurityContext.getTokenString();
+
+        //build the POST
+        System.out.println("About to Post");
+
+        HttpClient client = new DefaultHttpClient();
+        try {
+            HttpPost post = new HttpPost("https://j1mongo-thesteve0.rhcloud.com/ws/players");
+            post.addHeader("Authorization", "Bearer " + token);
+            post.addHeader("Content-Type", "application/json");
+                post.setEntity(new StringEntity(JsonSerialization.writeValueAsString(character.getAllAttributes())));
+                HttpResponse response = client.execute(post);
+                if (response.getStatusLine().getStatusCode() != 200) {
+                    throw new RuntimeException("request failed for some non-helpful reason");
+                }
+
+                HttpEntity entity = response.getEntity();
+                InputStream is = entity.getContent();
+                try {
+                    return org.apache.commons.io.IOUtils.toString(is);
+                } finally {
+                    is.close();
+                }
+
+        } finally {
+            client.getConnectionManager().shutdown();
+        }
+        /*
+        Client client = ClientBuilder.newClient();
+        WebTarget resourceTarget = client.target("https://j1mongo-thesteve0.rhcloud.com/ws/players");
+        Invocation.Builder invocationBuilder = resourceTarget.request("application/json").header("Authorization", "bearer " + token);
+        Invocation invocation = invocationBuilder.buildPost(Entity.json(character));
+        Response response = invocation.invoke();
+        */
+        //System.out.println("response code: " + response.getStatus());
+
+        /*
+
+        https://jax-rs-spec.java.net/nonav/2.0-SNAPSHOT/apidocs/javax/ws/rs/client/Client.html
+        https://jax-rs-spec.java.net/nonav/2.0-SNAPSHOT/apidocs/javax/ws/rs/client/WebTarget.html
+        https://jax-rs-spec.java.net/nonav/2.0-SNAPSHOT/apidocs/javax/ws/rs/client/Invocation.Builder.html
+         */
+        //return character.getAllAttributes();
+
+    }
+
+}
