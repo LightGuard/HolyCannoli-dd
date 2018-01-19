587ba7914de8a2a100f35b48f0f79c219b33ca2a
Steven Pousty <steve.pousty@gmail.com>
2018-01-19 13:10:03 -0800

    Fixing json serialization

--- src/main/java/org/openshift/webservice/Generator.java
+++ src/main/java/org/openshift/webservice/Generator.java
@@ -1,13 +1,13 @@
 package org.openshift.webservice;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.keycloak.KeycloakSecurityContext;
-import org.keycloak.util.JsonSerialization;
 import org.openshift.model.*;
 import org.openshift.model.Character;
 
@@ -60,19 +60,21 @@ public class Generator {
         HttpClient client = new DefaultHttpClient();
         try {
             HttpPost post = new HttpPost("http://mongoservice:8080/ws/players");
-                post.setEntity(new StringEntity(JsonSerialization.writeValueAsString(character.getAllAttributes())));
-                HttpResponse response = client.execute(post);
-                if (response.getStatusLine().getStatusCode() != 200) {
-                    throw new RuntimeException("request failed for some non-helpful reason");
-                }
+            GsonBuilder builder = new GsonBuilder();
+            Gson gson = builder.create();
+            post.setEntity(new StringEntity(gson.toJson(character.getAllAttributes())));
+            HttpResponse response = client.execute(post);
+            if (response.getStatusLine().getStatusCode() != 200) {
+                throw new RuntimeException("request failed for some non-helpful reason");
+            }
 
-                HttpEntity entity = response.getEntity();
-                InputStream is = entity.getContent();
-                try {
-                    return org.apache.commons.io.IOUtils.toString(is);
-                } finally {
-                    is.close();
-                }
+            HttpEntity entity = response.getEntity();
+            InputStream is = entity.getContent();
+            try {
+                return org.apache.commons.io.IOUtils.toString(is);
+            } finally {
+                is.close();
+            }
 
         } finally {
             client.getConnectionManager().shutdown();
